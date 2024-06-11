d32d8e20aa52c538ff7d5ebae2efe6e79b4f55aa
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-06-11 15:11:51 +0900

    Design: [home] 탑 배너 영역 구현
    1) 배경 이미지 삽입
    - 그라데이션 영역 추가
    
    2) 탑 배너 타이틀 삽입

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,27 +8,21 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className={styles.body_container}>
-      <div className={styles.intro}>
-        <h1 className="text-4xl font-bold">
-          우리는 모두 <span className="text-youth_color-m">'청춘'</span>입니다.
-        </h1>
-        <p className="mt-3">새로운 시작, 두려워 마세요.</p>
-        <p>
-          경험과 지혜가 더해진 지금, 당신의 새로운 <span className="text-youth_color-m font-bold">‘청춘’</span>을
-          응원합니다.
-        </p>
+      {/* Top-Banner 시작 */}
+      <div className={styles.top_banner}>
+        <div className={styles.banner_gradient}>
+          <div className={styles.top_banner_text}>
+            <p className={styles.banner_title}>
+              우리는 모두 <span className="text-youth_color-m">청춘</span>입니다.
+            </p>
+            <p className={styles.banner_sub_title}>새로운 시작, 두려워 마세요.</p>
+            <p className={styles.banner_sub_title}>경험과 지혜가 더해진 지금, 당신의 새로운 '청춘'을 응원합니다.</p>
+          </div>
+        </div>
       </div>
-      <ul className={styles.steps}>
-        <li>
-          <h1>1. 전체 경력 관리</h1>
-        </li>
-        <li>
-          <h1>2. 이력서 작성</h1>
-        </li>
-        <li>
-          <h1>3. 이력서 다운로드</h1>
-        </li>
-      </ul>
+      {/* Top-Banner 끝 */}
+      {/* Mid-Content 시작 */}
+      <div className={styles.mid_content}></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "main-top-banner": "url('/image/banner-3.jpg')"
+      }
+    },
     colors: {
       'youth_color': {
         'm': '#5170FD',
@@ -17,7 +21,8 @@ module.exports = {
         'gray': '#f2f2f2',
         'gray2': '#8d8d8d'
       },
-      'bg_color': '#F9F9F9'
+      'bg_color': '#F9F9F9',
+      'bg_gradient': '#000000'
     },
   },
   plugins: [],
