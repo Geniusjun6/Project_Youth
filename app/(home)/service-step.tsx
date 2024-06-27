971920fd974aa5d66126b75e948eb31c50479fc1
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-06-28 00:27:54 +0900

    Design: [home] 서비스 안내 부분 반응형 적용

--- app/(home)/service-step.tsx
+++ app/(home)/service-step.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import CTA_Btn from "../../public/image/CTA-btn.svg";
 
 /* ServiceStep에 들어갈 props 타입 정의 */
 interface ServiceStepProps {
@@ -15,20 +16,24 @@ export default function ServiceStep({ subTitle, mainTitle, subscription, href, i
   return (
     <Link
       href={href}
-      className="relative min-w-[400px] w-[480px] h-[350px] rounded-3xl shadow-md bg-bg_white transition-all duration-300 ease-in-out hover:w-[700px] hover:bg-youth_color-s group"
+      className="relative shadow-md bg-bg_white min-w-[366px] max-w-[500px] min-h-[156px] rounded-lg md:min-w-[400px] md:w-[480px] md:h-[350px] md:rounded-3xl md:transition-all md:duration-300 md:ease-in-out md:hover:w-[700px] md:hover:bg-youth_color-s group"
     >
-      <div className="absolute w-full h-full p-10 flex flex-col justify-between">
+      <div className="absolute w-full h-full p-5 flex flex-col justify-between md:p-10">
         <div>
-          <h3 className="font-semibold text-xl text-youth_color-s group-hover:text-text_color-gray ">{subTitle}</h3>
-          <h1 className="font-bold text-3xl mb-2 group-hover:text-text_color-gray">{mainTitle}</h1>
-          <p className="hidden group-hover:block group-hover:text-text_color-gray">{subscription}</p>
+          <h3 className="font-semibold text-sm md:text-xl text-youth_color-s md:group-hover:text-text_color-gray ">
+            {subTitle}
+          </h3>
+          <h1 className="font-bold md:text-3xl md:mb-2 md:group-hover:text-text_color-gray">{mainTitle}</h1>
+          <p className="text-[10px] block md:hidden md:group-hover:block md:group-hover:text-text_color-gray md:text-lg">
+            {subscription}
+          </p>
         </div>
         <div className="flex justify-between items-end">
           <div>
-            <Image src="/image/CTA-btn.png" alt="바로가기 버튼" width={50} height={50} layout="intrinsic" />
+            <Image className="w-6 h-6 md:w-16 md:h-16" src={CTA_Btn} alt="바로가기 버튼" />
           </div>
           <div>
-            <Image src={imageSrc} alt={imageAlt} width={100} height={100} layout="intrinsic" />
+            <Image className="w-16 h-16 md:w-32 md:h-32" src={imageSrc} alt={imageAlt} layout="intrinsic" />
           </div>
         </div>
       </div>
--- app/(home)/sevice-steps.tsx
+++ app/(home)/sevice-steps.tsx
@@ -1,15 +1,18 @@
 import ServiceStep from "./service-step";
+import career from "../../public/image/career.svg";
+import resume from "../../public/image/resume.svg";
+import pdf from "../../public/image/pdf.svg";
 
 export default function ServiceSteps() {
   return (
-    <div className="flex justify-end mt-20 mb-10 space-x-4">
+    <div className="flex flex-col space-y-2 mt-6 mb-5 items-center md:flex-row md:justify-end md:mt-20 md:mb-10 md:space-x-4">
       {/* 경력관리 */}
       <ServiceStep
         href="/career"
         subTitle="Career Management"
         mainTitle="AI와 함께하는 전체 경력 관리"
         subscription="간편한 경력 불러오기와 AI를 통해 쉽게 경력을 관리하세요."
-        imageSrc="/image/Career.png"
+        imageSrc={career}
         imageAlt="경력관리 아이콘"
       />
       {/* 이력서 작성 */}
@@ -18,7 +21,7 @@ export default function ServiceSteps() {
         subTitle="Completion of Resume"
         mainTitle="간편한 이력서 작성"
         subscription="지원 직무와 연관된 라벨의 경력을 선택하여 이력서를 작성해요."
-        imageSrc="/image/resume.png"
+        imageSrc={resume}
         imageAlt="이력서 작성 아이콘"
       />
       {/* 이력서 다운로드 */}
@@ -27,7 +30,7 @@ export default function ServiceSteps() {
         subTitle="Download Resume"
         mainTitle="이력서 다운로드"
         subscription="작성한 이력서를 PDF 형태로 다운받을 수 있어요."
-        imageSrc="/image/pdf.png"
+        imageSrc={pdf}
         imageAlt="이력서 다운로드 아이콘"
       />
     </div>
