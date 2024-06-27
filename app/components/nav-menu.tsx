e2204ef115786ec499836bda143ebd8c3532f0f5
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-06-27 22:52:27 +0900

    Design: [home] Nav-bar 반응형 적용

--- app/components/nav-menu.tsx
+++ app/components/nav-menu.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 interface NavMenuProps {
   menuName: string;
   href: string;
+  css?: string;
 }
 
-export default function NavMenu({ menuName, href }: NavMenuProps) {
+export default function NavMenu({ menuName, href, css }: NavMenuProps) {
   return (
     <li>
       <Link
-        className="block h-full content-center rounded-md px-4 hover:bg-youth_color-s/45 hover:font-bold cursor-pointer"
+        className={`block h-full content-center rounded-md px-4 hover:bg-youth_color-s/45 hover:font-bold cursor-pointer ${css}`}
         href={href}
       >
         {menuName}
--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -4,43 +4,51 @@ import Link from "next/link";
 import { useState } from "react";
 import NavMenu from "./nav-menu";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../public/image/logo.svg";
 
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
+  const toggleMenu: () => void = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <nav className="z-40 fixed top-0 left-0 right-0 shadow-md bg-youth_color-m p-4 mb-4 content-center text-text_color-gray text-xl">
-      <ul className="flex justify-between w-11/12 mx-auto;">
-        <div className="flex content-center space-x-10 shrink-0;">
-          <li>
+    <nav className="z-40 fixed top-0 left-0 right-0 shadow-md bg-youth_color-m p-4 mb-4 content-center text-text_color-gray text-sm md:text-xl">
+      <ul className="flex justify-between w-11/12 min-w-[320px] mx-auto ">
+        <div className="flex content-center space-x-10">
+          <li className="flex justify-center items-center">
             <Link href="/">
-              <Image src="/image/logo_white.png" alt="Logo" width={70} height={70} />
+              <Image className="md:w-[60px] md:h-[60px] w-12 h-12" src={logo} alt="로고" />
             </Link>
           </li>
-          <button className="md:hidden block;" onClick={toggleMenu}>
-            <svg
-              className="w-6 h-6;"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
-          </button>
         </div>
-        <div className={`flex content-center space-x-10 shrink-0 ${isOpen ? "show" : "hide"}`}>
+        <div className="hidden md:flex content-center space-x-10 shrink-0">
           <NavMenu menuName={"경력 관리하기"} href={"/career"} />
           <NavMenu menuName={"이력서 작성하기"} href={"/resume"} />
           <NavMenu menuName={"문의하기"} href={"/contact"} />
         </div>
-        <div className={`flex content-center space-x-10 shrink-0 ${isOpen ? "show" : "hide"}`}>
-          <NavMenu menuName={"로그인"} href={"/sign-in"} />
+        <div className="flex space-x-3">
+          <div className="flex">
+            <NavMenu menuName={"로그인"} href={"/sign-in"} />
+          </div>
+          <button className="flex md:hidden text-3xl justify-center items-center p-2">
+            {isOpen ? (
+              <FontAwesomeIcon className="text-2xl" onClick={toggleMenu} icon={faX} />
+            ) : (
+              <FontAwesomeIcon onClick={toggleMenu} icon={faBars} />
+            )}
+          </button>
         </div>
+      </ul>
+      {/* 모바일 메뉴 */}
+      <ul className={`${isOpen ? "flex" : "hidden"} flex-col md:hidden px-3 py-2 `}>
+        <hr className="mb-4" />
+        <NavMenu menuName={"경력 관리하기"} href={"/career"} css="p-4" />
+        <NavMenu menuName={"이력서 작성하기"} href={"/resume"} css="p-4" />
+        <NavMenu menuName={"문의하기"} href={"/contact"} css="p-4" />
       </ul>
     </nav>
   );
