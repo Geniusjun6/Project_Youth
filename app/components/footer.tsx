c1a49e1c0c1fe84c4f7fddc3f3b4bd7b824c7b57
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-06-28 00:39:38 +0900

    Design: [home] Footer 반응형 적용

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import FooterMenu from "./footer-menu";
+import logo from "../../public/image/logo.svg";
 
 export default function Footer() {
   return (
@@ -9,17 +10,17 @@ export default function Footer() {
         <div className="flex content-center justify-between">
           <div>
             <Link href="/">
-              <Image src="/image/logo_white.png" alt="Logo_White" width={50} height={50} />
+              <Image className="w-10 h-10  md:w-12 md:h-12" src={logo} alt="Logo_White" />
             </Link>
           </div>
-          <ul className="flex space-x-10 items-center">
+          <ul className="flex space-x-3 text-sm md:text-md md:space-x-10 items-center">
             <FooterMenu menuName={"회사소개"} href={"/about-us"} />
             <FooterMenu menuName={"문의하기"} href={"/contact"} />
             <FooterMenu menuName={"이용약관"} href={"/policy/service"} />
             <FooterMenu menuName={"개인정보 처리방침"} href={"/policy/privacy"} />
           </ul>
         </div>
-        <div className="mt-4">
+        <div className="text-[11px] md:text-md mt-4">
           <span>대표이사 문준식</span>
           <span className="mx-2">|</span>
           <span>이메일 : youth@theyouth.com</span>
@@ -29,6 +30,7 @@ export default function Footer() {
           <p />
           <span>사업자등록번호 : 000-00-0000</span> <span className="mx-2">|</span>
           <span>통신판매번호 : 2024-서울강남-0000</span> <span className="mx-2">|</span>
+          <p className="md:hidden" />
           <span>유료직업소개사업등록번호 : (국내) 제2020-3230259-14-5-00018호</span>
         </div>
       </div>
