40376806721638318ce1023fd13f15bafd83c61d
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-06-27 23:34:50 +0900

    Design: [home] 중간 텍스트 부분 반응형 적용

--- app/(home)/mid-text.tsx
+++ app/(home)/mid-text.tsx
@@ -1,8 +1,8 @@
 export default function MidText() {
   return (
-    <div className="mt-60 ml-20">
-      <p className="text-2xl font-bold text-text_color-gray2">앞으로 더욱 더 빛날 청춘을 함께하겠습니다.</p>
-      <div className="font-bold text-5xl space-y-3 mt-5">
+    <div className="mt-6 ml-6 md:mt-60 md:ml-20">
+      <p className="text-sm text-text_color-gray2 font-bold md:text-2xl">앞으로 더욱 더 빛날 청춘을 함께하겠습니다.</p>
+      <div className="text-md font-bold mt-3 md:mt-5 md:text-5xl md:space-y-3">
         <p>
           AI와 함께하는 <span className="text-youth_color-m">전체 경력관리</span>와
         </p>
