a228623c42643f08885e8bea0fd75e25f3ce83e0
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-07-07 14:56:35 +0900

    Style: [user/frontend] 화면 넓이 수정
    - 화면 넓이에서 가로 스크롤이 되는 경우가 있어서 해당 부분 수정

--- frontend/app/sign-up/(components)/page.tsx
+++ frontend/app/sign-up/(components)/page.tsx
@@ -1,26 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import { Provider } from "react-redux";
 import Step1 from "./step1";
+import Step2 from "./step2";
+import BottomButton from "./bottom-button";
+import { store } from "../util/store";
 
 export default function SignUp() {
-  const [step, setStep] = useState(1);
-
-  /** 다음 Step으로 설정하는 함수 */
-  const toNextStep = () => {
-    setStep((acc) => acc + 1);
-  };
-
-  /** 이전 Step으로 설정하는 함수 */
-  const toPrevStep = () => {
-    setStep((acc) => acc - 1);
-  };
-
   return (
-    <div className="w-11/12 h-full mx-auto">
-      <h1 className="w-full text-center text-lg md:text-2xl font-bold">회원가입</h1>
-      {step === 1 && <Step1 toNextStep={toNextStep} />}
-      {step === 2 && null}
-    </div>
+    <Provider store={store}>
+      <div className="w-11/12 h-full mx-auto">
+        <h1 className="w-full text-center text-lg md:text-2xl font-bold">회원가입</h1>
+        <Step1 />
+        <Step2 />
+        <BottomButton />
+      </div>
+    </Provider>
   );
 }
