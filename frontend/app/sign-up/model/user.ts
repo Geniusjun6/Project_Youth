befedb5fb7750e15e183d8d3fa15f4f338ccb53b
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-07-07 15:00:31 +0900

    Fix: [user/frontend] 휴대폰 번호 입력 시 이름 수정되는 버그 수정
    - 기존 코드에서 휴대폰 번호를 입력하면 이름이 phone으로 수정됨
    - 변수 이름이 구분되지 않아서 발생하는 문제로 확인이하고 유저 인터페이스 내 이름을 userName으로 수정

--- frontend/app/sign-up/model/user.ts
+++ frontend/app/sign-up/model/user.ts
@@ -2,24 +2,7 @@ export interface UserProps {
   email: string;
   password: string;
   passwordCheck: string;
-  name: string;
+  userName: string;
   phone: string;
   gender: string;
-}
-
-export class User {
-  email: string;
-  password: string;
-  passwordCheck: string;
-  name: string;
-  phone: string;
-  gender: string;
-  constructor({ email, password, passwordCheck, name, phone, gender }: UserProps) {
-    this.email = email;
-    this.password = password;
-    this.passwordCheck = passwordCheck;
-    this.name = name;
-    this.phone = phone;
-    this.gender = gender;
-  }
 }
