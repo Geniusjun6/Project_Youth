4800504a69cce816fbd77c970ca5c79d18085cb3
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-07-18 23:28:31 +0900

    Fix: [user/signin] GlobalFilter 필요 없는 코드 삭제

--- backend/src/common/filter/global.filter.ts
+++ backend/src/common/filter/global.filter.ts
@@ -1,9 +1,8 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 
 @Catch()
 export class GlobalExceptionsFilter implements ExceptionFilter {
-  constructor(private readonly configService: ConfigService) {}
+  constructor() {}
 
   async catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
@@ -11,12 +10,11 @@ export class GlobalExceptionsFilter implements ExceptionFilter {
 
     if (exception instanceof HttpException) {
       return response.status(exception.getStatus()).json(exception.getResponse());
-    } else {
-      // 기본 에러 응답
-      response.status(500).json({
-        success: false,
-        message: "서버에 문제가 있습니다. 관리자에게 문의하세요."
-      });
     }
+    // 기본 에러 응답
+    response.status(500).json({
+      success: false,
+      message: "서버에 문제가 있습니다. 관리자에게 문의하세요."
+    });
   }
 }
